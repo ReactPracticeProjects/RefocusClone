37f0df7a053635e8d46879936a9c8dbf93431557
lordsainath <lordsainath8@gmail.com>
2025-01-26 01:37:22 +0530

    Navbar Added To The Site

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button() {
+  return (
+   <>
+    
+    <button className='bg-white rounded-2xl py-1 text-zinc-800 px-3'>
+      <span className='text-sm mr-1.5'>Start a Project</span>  <i class="ri-play-line"></i>
+    </button>
+    
+   </>
+  )
+}
+
+export default Button
